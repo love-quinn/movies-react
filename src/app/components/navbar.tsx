ec0009febc0fde8939bf4cdaf2cf211c6f41cbc2
love-quinn <lucaslucal75@gmail.com>
2025-09-01 18:43:59 -0300

    feat:started navbar

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return ( 
+        <nav className="bg-gray-800 p-4">
+            <ul>
+                <li>Filmes</li>
+            </ul>
+        </nav>
+     );
+}
+ 
+export default Navbar;
